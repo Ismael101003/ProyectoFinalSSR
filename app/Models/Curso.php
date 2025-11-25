376253e003a755914d581e40db763c8ffde8ae0e
ismael101003 <ismael101003@gmail.com>
2025-11-24 18:51:01 -0600

    FEAT💫: Implementar controlador y solicitudes para gestión de cursos y valoraciones

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    public function valoraciones()
+    {
+        return $this->hasMany(Valoracion::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- app/Models/Valoracion.php
+++ app/Models/Valoracion.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Valoracion extends Model
 {
     protected $table = 'resenas';
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function curso() {
+        return $this->belongsTo(Curso::class);
+    }
 }
